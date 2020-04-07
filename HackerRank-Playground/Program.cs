47425ab24a800c08e4eb85972f73addfc45841f7
vasilisdimitriou <vasilisdmr@gmail.com>
2020-04-07 19:39:32 +0300

    Day 5: Loops

--- HackerRank-Playground/Program.cs
+++ HackerRank-Playground/Program.cs
@@ -6,7 +6,12 @@ namespace HackerRank_Playground
     {
         static void Main(string[] args)
         {
-            
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{n} x {i} = {n*i}\n");
+            }
         }
     }
 }
