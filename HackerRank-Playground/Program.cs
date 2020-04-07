15cea7366e5f8b43b3d4a007e43afa24e10bcd08
vasilisdimitriou <vasilisdmr@gmail.com>
2020-04-07 20:42:51 +0300

    Day 6: Let's Review

--- HackerRank-Playground/Program.cs
+++ HackerRank-Playground/Program.cs
@@ -6,12 +6,9 @@ namespace HackerRank_Playground
     {
         static void Main(string[] args)
         {
-            int n = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 1; i <= 10; i++)
-            {
-                Console.WriteLine($"{n} x {i} = {n*i}\n");
-            }
+            int numberOfStrings = Convert.ToInt32(Console.ReadLine());
+            
+            Solution.evenOddChars(numberOfStrings);
         }
     }
 }
