fd1649f9e45cebd7cd174f48fa51784c4bc32ee9
vasilisdimitriou <vasilisdmr@gmail.com>
2020-04-13 21:58:22 +0300

    Added person base class

---
+++ HackerRank-Playground/Inheritane/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace HackerRank_Playground.Inheritane
+{
+    public class Person
+    {
+		protected string firstName;
+		protected string lastName;
+		protected int id;
+
+		public Person() { }
+		public Person(string firstName, string lastName, int identification)
+		{
+            this.firstName = firstName;
+			this.lastName = lastName;
+			id = identification;
+		}
+		public void printPerson()
+		{
+			Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + id);
+		}
+	}
+}
