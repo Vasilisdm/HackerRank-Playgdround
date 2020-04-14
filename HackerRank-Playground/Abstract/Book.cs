b707b215a8cc210599a7f63f7afa0f5afa7c747d
vasilisdimitriou <vasilisdmr@gmail.com>
2020-04-14 21:22:26 +0300

    Added abstract book class

---
+++ HackerRank-Playground/Abstract/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace HackerRank_Playground.Abstract
+{
+    public abstract class Book
+    {
+
+        protected string title;
+        protected string author;
+
+        public Book(string t, string a)
+        {
+            title = t;
+            author = a;
+        }
+        public abstract void display();
+
+
+    }
+}
