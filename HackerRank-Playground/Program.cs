f703b7e10e02d6ed823a69e96b08086f99e1eb49
vasilisdimitriou <vasilisdmr@gmail.com>
2020-04-14 21:35:43 +0300

    Day 13: Abstract Classes

--- HackerRank-Playground/Program.cs
+++ HackerRank-Playground/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using HackerRank_Playground.Abstract;
 using HackerRank_Playground.Inheritane;
 
 namespace HackerRank_Playground
@@ -8,21 +9,11 @@ namespace HackerRank_Playground
     {
         static void Main(string[] args)
         {
-			string[] inputs = Console.ReadLine().Split();
-			string firstName = inputs[0];
-			string lastName = inputs[1];
-			int id = Convert.ToInt32(inputs[2]);
-			int numScores = Convert.ToInt32(Console.ReadLine());
-			inputs = Console.ReadLine().Split();
-			int[] scores = new int[numScores];
-			for (int i = 0; i < numScores; i++)
-			{
-				scores[i] = Convert.ToInt32(inputs[i]);
-			}
-
-			Student s = new Student(firstName, lastName, id, scores);
-			s.printPerson();
-			Console.WriteLine("Grade: " + s.Calculate() + "\n");
-		}
+            String title = Console.ReadLine();
+            String author = Console.ReadLine();
+            int price = Int32.Parse(Console.ReadLine());
+            MyBook new_novel = new MyBook(title, author, price);
+            new_novel.display();
+        }
     }
 }
