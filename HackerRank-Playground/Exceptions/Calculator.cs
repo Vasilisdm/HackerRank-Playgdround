b99144ee71d7f53f9d59b60630d73a411671f4a3
vasilisdimitriou <vasilisdmr@gmail.com>
2020-04-18 21:07:40 +0300

    Day 17: More Exceptions

---
+++ HackerRank-Playground/Exceptions/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace HackerRank_Playground.Exceptions
+{
+    public class Calculator
+    {
+        public int Power(int n, int p)
+        {
+            if (n<0 | p<0)
+            {
+                throw new Exception("n and p should be non-negative");
+            }
+
+            if (p==0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Power(n, p - 1);
+            }
+        }
+    }
+}
--- HackerRank-Playground/Program.cs
+++ HackerRank-Playground/Program.cs
@@ -8,9 +8,24 @@ namespace HackerRank_Playground
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Insert a number");
-            string S = Console.ReadLine();
-            StringToInt.ConvertStringToInt(S);
+            Calculator myCalculator = new Calculator();
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                string[] num = Console.ReadLine().Split();
+                int n = int.Parse(num[0]);
+                int p = int.Parse(num[1]);
+                try
+                {
+                    int ans = myCalculator.Power(n, p);
+                    Console.WriteLine(ans);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+
+                }
+            }
         }
     }
 }
