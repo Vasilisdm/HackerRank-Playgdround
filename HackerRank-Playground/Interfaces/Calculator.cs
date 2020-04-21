1670d3c2f3ce0c1d547c4b8432723fca311f6bdf
vasilisdimitriou <vasilisdmr@gmail.com>
2020-04-21 21:54:03 +0300

    Day 19: Interfaces

---
+++ HackerRank-Playground/Interfaces/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using HackerRank_Playground.Interface;
+
+namespace HackerRank_Playground.Interfaces
+{
+    public class Calculator : AdvancedArithmetic
+    {
+        private int[] divisorArray = new int[100];
+
+        public int divisorSum(int n)
+        {
+            int sum = 0;
+            int j = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i ==0)
+                {
+                    divisorArray[j++] = i;
+                }
+            }
+
+            foreach (var divisor in divisorArray)
+            {
+                sum += divisor;
+            }
+
+            return sum;
+        }
+    }
+}
--- HackerRank-Playground/Program.cs
+++ HackerRank-Playground/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using HackerRank_Playground.QueuesandStacks;
+using HackerRank_Playground.Interface;
+using HackerRank_Playground.Interfaces;
 
 namespace HackerRank_Playground
 {
@@ -7,37 +8,10 @@ namespace HackerRank_Playground
     {
         static void Main(string[] args)
         {
-            string s = Console.ReadLine();
-
-            QueueStackHelper obj = new QueueStackHelper();
-
-            foreach (char c in s)
-            {
-                obj.pushCharacter(c);
-                obj.enqueueCharacter(c);
-            }
-
-            bool isPalindrome = true;
-
-            for (int i = 0; i < s.Length / 2; i++)
-            {
-                if (obj.popCharacter() != obj.dequeueCharacter())
-                {
-                    isPalindrome = false;
-
-                    break;
-                }
-            }
-
-            // finally print whether string s is palindrome or not.
-            if (isPalindrome)
-            {
-                Console.Write("The word, {0}, is a palindrome.", s);
-            }
-            else
-            {
-                Console.Write("The word, {0}, is not a palindrome.", s);
-            }
+            int n = Int32.Parse(Console.ReadLine());
+            AdvancedArithmetic myCalculator = new Calculator();
+            int sum = myCalculator.divisorSum(n);
+            Console.WriteLine("I implemented: AdvancedArithmetic\n" + sum);
         }
     }
 }
