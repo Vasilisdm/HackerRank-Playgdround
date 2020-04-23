97655ff4ee6be43dacc5607219f3a5bb7fd23c99
vasilisdimitriou <vasilisdmr@gmail.com>
2020-04-23 20:42:14 +0300

    Day 21: Generics

---
+++ HackerRank-Playground/Generics/GenericArray.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace HackerRank_Playground.Generics
+{
+    public static class GenericArray<T>
+    {
+        public static void PrintArray<T>(T[] arr)
+        {
+            foreach (T element in arr)
+            {
+                Console.WriteLine($"{element}");
+            }
+        }
+    }
+}
--- HackerRank-Playground/Program.cs
+++ HackerRank-Playground/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using HackerRank_Playground.BubbleSort;
+using HackerRank_Playground.Generics;
 
 namespace HackerRank_Playground
 {
@@ -7,11 +8,22 @@ namespace HackerRank_Playground
     {
         static void Main(string[] args)
         {
-            int n = Convert.ToInt32(Console.ReadLine());
-            string[] a_temp = Console.ReadLine().Split(' ');
-            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+			int n = Convert.ToInt32(Console.ReadLine());
+			int[] intArray = new int[n];
+			for (int i = 0; i < n; i++)
+			{
+				intArray[i] = Convert.ToInt32(Console.ReadLine());
+			}
 
-            BubbleSort.Bbl.BblSort(a);
-        }
+			n = Convert.ToInt32(Console.ReadLine());
+			string[] stringArray = new string[n];
+			for (int i = 0; i < n; i++)
+			{
+				stringArray[i] = Console.ReadLine();
+			}
+
+			GenericArray<int>.PrintArray<Int32>(intArray);
+			GenericArray<string>.PrintArray<string>(stringArray);
+		}
     }
 }
