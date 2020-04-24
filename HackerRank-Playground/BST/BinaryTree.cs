ba0489bf4e94f7f37e5750b5e85d34301e1d1f14
Vasileios Dimitriou <vasilisdmr@gmail.com>
2020-04-24 17:09:10 +0300

    LevelOrder traversal method
    Added the LevelOrder traversal method which will be used to iterate each node of the tree once.

--- HackerRank-Playground/BST/BinaryTree.cs
+++ HackerRank-Playground/BST/BinaryTree.cs
@@ -3,6 +3,8 @@ namespace HackerRank_Playground.BST
 {
     public static class BinaryTree
     {
+        static Queue<Node> nodeQueue = new Queue<Node>();
+        
         public static int GetHeight(Node root)
         {
             if (root == null)
@@ -10,6 +12,26 @@ namespace HackerRank_Playground.BST
                 return -1;
             }
             return 1 + (Math.Max(GetHeight(root.left), GetHeight(root.right)));
+        }
+        
+        public static void LevelOrder(Node root)
+        {
+            nodeQueue.Enqueue(root);
+            while (nodeQueue.Count > 0)
+            {
+                var node = nodeQueue.Dequeue();
+                Console.Write(node.data + " ");
+
+                if (node.left != null)
+                {
+                    nodeQueue.Enqueue(node.left);
+                }
+
+                if (node.right != null)
+                {
+                    nodeQueue.Enqueue(node.right);
+                }
+            }
         }
 
         public static Node Insert(Node root, int data)
