f7e53aae6fbc4bc1ff6877a5493c62d73895c8c1
vasilisdimitriou <vasilisdmr@gmail.com>
2020-04-24 22:52:36 +0300

    Counting Valleys

---
+++ HackerRank-Playground/WarmUP/Hike.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace HackerRank_Playground.WarmUP
+{
+    public static class Hike
+    {
+        public static int CountValleys(int n, string s)
+        {
+            int altitude = 0;
+            int valleyCounter = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (s[i] == 'U')
+                {
+                    if (++altitude == 0)
+                    {
+                        valleyCounter++;
+                    }
+                }
+                else
+                {
+                    altitude--;
+                }
+            }
+            return valleyCounter;
+        }
+    }
+}
